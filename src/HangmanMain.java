36c0036803d43730b877e6749c693e91e87b82ac
ruslanabdulin1985 <ruslanabdulin1985@gmail.com>
2019-02-05 14:58:50 +0100

    I have made 2 stages

---
+++ src/HangmanMain.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author user
+ *
+ */
+public class HangmanMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
